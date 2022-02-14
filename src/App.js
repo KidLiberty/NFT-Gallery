37c543685dcb749e9da0f489da9bfa048b9c777f
Brandon Stone <brandonrstone@gmail.com>
2022-02-14 11:46:43 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [gallery, setGallery] = useState([])
+  const [savedGallery, setSavedGallery] = useState([])
+  const [id, setId] = useState(0)
+
+  function savePhotos() {
+    let newGallery = gallery.map(image => ({ ...image, isLoading: true }))
+    setSavedGallery([...newGallery, ...savedGallery])
+    newGallery = gallery.map(image => ({ ...image, isLoading: false }))
+    setTimeout(() => {
+      setSavedGallery(
+        [...newGallery, ...savedGallery].sort((a, b) => {
+          return b.id - a.id
+        })
+      )
+    }, 1000)
+    setGallery([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className='btn-container'>
+        <button
+          onClick={() => {
+            const sort = [
+              ...gallery,
+              {
+                id: id,
+                image: <img src={require('./images/samo.png')} alt='Samo Pic' />
+              }
+            ].sort((a, b) => {
+              return b.id - a.id
+            })
+            setGallery(sort)
+            setId(id + 1)
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Take Photo
+        </button>
+        <button onClick={() => savePhotos()}>Save Photos</button>
+      </div>
+      <div className='gallery'>
+        {gallery.map(object => (
+          <div key={object.id} className='photo-container'>
+            <div className='photo-id-container'>
+              <div className='photo-id'>{object.id + 1}</div>
+            </div>
+            <img src={require('./images/samo.png')} alt='Samo Pic' />
+          </div>
+        ))}
+        {savedGallery.map(object => (
+          <div key={object.id} className='photo-container'>
+            <div className='photo-id-container'>
+              <span className='photo-id'>{object.id + 1}</span>
+            </div>
+            <img
+              src={require('./images/samo.png')}
+              alt='Samo Pic'
+              style={
+                object.isLoading
+                  ? { border: '1px solid yellow' }
+                  : { border: '1px solid green' }
+              }
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
